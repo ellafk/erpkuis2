890c99b2e86f2bde058bed2eae33198122f12471
ellafk <ellafitrikusumaningrum@gmail.com>
2021-12-23 01:22:47 +0700

    first

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
- $db = mysqli_connect('10.0.0.23', 'admin', '@Admin123_') or
+ $db = mysqli_connect('10.0.0.49', 'admin', '@Admin123_') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'erp3' ) or die(mysqli_error($db));
 ?>
